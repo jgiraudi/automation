9a6c9bf31e0b94cdf54270e000dc7435dc785bf8
Julio Giraudi <50597155+jgiraudi@users.noreply.github.com>
2021-04-26 10:38:06 -0300

    Create impresoras.ps1

---
+++ impresoras.ps1
@@ -0,0 +1,11 @@
+Write-Host "Instalando Impresoras"
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Write-Host "Impresora Cocina"
+Add-PrinterPort -Name "cocina" -PrinterHostAddress "192.168.10.117"
+Add-Printer -Name "cocina" -DriverName "Generic / Text Only" -PortName "cocina"
+Write-Host "Impresora Barra"
+Add-PrinterPort -Name "barra" -PrinterHostAddress "192.168.10.116"
+Add-Printer -Name "barra" -DriverName "Generic / Text Only" -PortName "barra"
+Write-Host "Impresora Control
+Add-PrinterPort -Name "control" -PrinterHostAddress "192.168.10.115"
+Add-Printer -Name "control" -DriverName "Generic / Text Only" -PortName "control"
