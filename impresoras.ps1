294ef7e7cc4d8bb549416c86b004c66b3f0d5dae
Julio Giraudi <50597155+jgiraudi@users.noreply.github.com>
2021-04-26 10:57:35 -0300

    Update impresoras.ps1

--- impresoras.ps1
+++ impresoras.ps1
@@ -6,7 +6,7 @@ Add-Printer -Name "cocina" -DriverName "Generic / Text Only" -PortName "cocina"
 Write-Host "Impresora Barra"
 Add-PrinterPort -Name "barra" -PrinterHostAddress "192.168.10.116"
 Add-Printer -Name "barra" -DriverName "Generic / Text Only" -PortName "barra"
-Write-Host "Impresora Control
+Write-Host "Impresora Control"
 Add-PrinterPort -Name "control" -PrinterHostAddress "192.168.10.115"
 Add-Printer -Name "control" -DriverName "Generic / Text Only" -PortName "control"
 
