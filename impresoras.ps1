fc79e774b5dee0f1c8a83f4992efbf14478bb239
Julio Giraudi <50597155+jgiraudi@users.noreply.github.com>
2021-04-26 13:13:08 -0300

    Update impresoras.ps1

--- impresoras.ps1
+++ impresoras.ps1
@@ -1,13 +1,12 @@
 Write-Host "Instalando Impresoras"
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Add-PrinterDriver -Name "Generic / Text Only"
-Write-Host "Impresora Cocina"
+Write-Host "Impresora Cocina - 117"
 Add-PrinterPort -Name "cocina" -PrinterHostAddress "192.168.10.117"
 Add-Printer -Name "cocina" -DriverName "Generic / Text Only" -PortName "cocina"
-Write-Host "Impresora Barra"
+Write-Host "Impresora Barra - 116"
 Add-PrinterPort -Name "barra" -PrinterHostAddress "192.168.10.116"
 Add-Printer -Name "barra" -DriverName "Generic / Text Only" -PortName "barra"
-Write-Host "Impresora Control"
+Write-Host "Impresora Control - 115"
 Add-PrinterPort -Name "control" -PrinterHostAddress "192.168.10.115"
 Add-Printer -Name "control" -DriverName "Generic / Text Only" -PortName "control"
-
