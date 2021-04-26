f7bfe3da01797fb8537a0b74711cc2580041f79c
Julio Giraudi <50597155+jgiraudi@users.noreply.github.com>
2021-04-26 10:53:06 -0300

    Update impresoras.ps1

--- impresoras.ps1
+++ impresoras.ps1
@@ -9,6 +9,4 @@ Add-Printer -Name "barra" -DriverName "Generic / Text Only" -PortName "barra"
 Write-Host "Impresora Control
 Add-PrinterPort -Name "control" -PrinterHostAddress "192.168.10.115"
 Add-Printer -Name "control" -DriverName "Generic / Text Only" -PortName "control"
-#Write-Host "configura tamaño de papel"
-#$Printers = Get-Printer * 
-#Foreach ($Printer in $Printers){Set-PrintConfiguration -PrinterName $Printer.name -PaperSize MaxiRest}
+
